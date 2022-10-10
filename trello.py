cf6d053b8068afc234c796b1ec10a2750dd0d9fc
paulomtts <paulomtts@outlook.com>
2022-10-10 14:04:30 -0300

    custom field rework

--- trello.py
+++ trello.py
@@ -797,10 +797,7 @@ class ModifiedList(list):
                 raise TypeError("Incorrect mix of types. Provide a dictionary followed by strings.")
 
 
-key = '637c56e248984ec499c0361ccb63f695'
-token = '44162f9fa00913303974d79d1151c3414ee0d9978f2e6720ebff65adf5afe3bf'
-brd_id = '62221524f3b7441300da7a88'
-brd_name = 'Agendas (Novo)'
+
 crd: Card; fld_item: CustomFieldItem
 
 start = time.perf_counter()
